daa3449afccd13831276a5b6fb346c3158717d97
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-10 13:47:53 -0600

    post request

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -11,7 +11,7 @@ class PostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class PostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'min:4', 'max:40'],
+            'content' => ['required', 'string', 'min:6', 'max:255'],
+            'image' => ['required', 'image', 'mimes:jpg,png,jpeg,webp', 'max:4096'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'user_id' => ['required', 'exists:categories,id'],
         ];
     }
 }
