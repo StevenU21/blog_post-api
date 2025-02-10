4c82bf7874825e7adcba41ad4f8dcbd2a2471fc5
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-10 00:18:21 -0600

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LabelController;
 use App\Http\Controllers\PostController;
@@ -9,6 +11,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('labels', LabelController::class);
