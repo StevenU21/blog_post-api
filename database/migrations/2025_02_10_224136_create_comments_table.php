28831c90b39b80afcf51b6bb3faf49dd29bddd34
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-10 16:49:00 -0600

    comments table

--- database/migrations/2025_02_10_224136_create_comments_table.php
+++ database/migrations/2025_02_10_224136_create_comments_table.php
@@ -4,15 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->text('content');
+
+            //relations
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
