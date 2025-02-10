447be1836a2955cd3dbedefd36faefea7cf17f39
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-09 23:52:21 -0600

    feat: Label request, resource, factory, seeder and index controller function

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\LabelResource;
+use App\Models\Label;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class LabelController extends Controller
 {
-    //
+    public function index(Request $request): AnonymousResourceCollection
+    {
+        $per_page = $request->get('per_page', 10);
+
+        $labels = Label::latest()->paginate($per_page);
+
+        return LabelResource::collection($labels);
+    }
 }
