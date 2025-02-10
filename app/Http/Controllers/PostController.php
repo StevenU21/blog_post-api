6228352fecf622fd8e024adb616ba264a801bd8c
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-10 13:49:56 -0600

    post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PostController extends Controller
 {
-    //
+    public function index(Request $request): AnonymousResourceCollection
+    {
+        $per_page = $request->get('per_page', 10);
+        $posts = Post::latest()->paginate($per_page);
+        return PostResource::collection($posts);
+    }
 }
