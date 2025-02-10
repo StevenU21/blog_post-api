fd65493935ab56545e6ef6f4f511cc08e9ad10d7
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-10 00:19:40 -0600

    auth middleware

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,17 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LabelController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('labels', LabelController::class);
-Route::apiResource('posts', PostController::class);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('labels', LabelController::class);
+    Route::apiResource('posts', PostController::class);
+});
+
