88fdc2231563767089aa7e0c1cc7cda445d83959
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-11 21:18:02 -0600

    comment store

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
@@ -20,5 +22,15 @@ class CommentController extends Controller
     {
         $comments = Comment::where('post_id', '=', $id)->with('user', 'post')->latest()->get();
         return CommentResource::collection($comments);
+    }
+
+    public function store(CommentRequest $request, Post $post): CommentResource
+    {
+        $comment = Comment::create($request->validated() + [
+            'post_id' => $post,
+            'user_id' => Auth::id()
+        ]);
+
+        return new CommentResource($comment);
     }
 }
