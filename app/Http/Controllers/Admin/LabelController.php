271577f47709f622d8e04b6e3b86fb22e6e3e56d
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-13 21:13:32 -0600

    refactor admin folder

--- app/Http/Controllers/Admin/LabelController.php
+++ app/Http/Controllers/Admin/LabelController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\LabelRequest;
 use App\Http\Resources\LabelResource;
 use App\Http\Resources\PostResource;
