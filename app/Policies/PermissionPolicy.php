ca5bedcd7fea14ad5dc534b4ee68d2c62c1d40f6
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-13 21:24:47 -0600

    role and permission policies

---
+++ app/Policies/PermissionPolicy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PermissionPolicy
+{
+    use HandlesAuthorization;
+    public function before(User $user, $ability)
+    {
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
+    public function viewAny(User $user): bool
+    {
+        if (!$user->hasPermissionTo('read permissions')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function view(User $user): bool
+    {
+        if (!$user->hasPermissionTo('read permissions')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function assignPermissions(User $user): bool
+    {
+        if (!$user->hasPermissionTo('assign permissions')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function revokePermissions(User $user): bool
+    {
+        if (!$user->hasPermissionTo('revoke permissions')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+}
---
+++ app/Policies/RolePolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RolePolicy
+{
+    use HandlesAuthorization;
+    public function before(User $user, $ability)
+    {
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
+    public function viewAny(User $user): bool
+    {
+        if (!$user->hasPermissionTo('view roles')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function view(User $user): bool
+    {
+        if (!$user->hasPermissionTo('view roles')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function assignRole(User $user): bool
+    {
+        if (!$user->hasPermissionTo('assign roles')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+}
