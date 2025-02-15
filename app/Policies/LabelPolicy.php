b5615f3d4223627ddec1a3c4bac2549172038ebd
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-14 22:04:42 -0600

    refactor label policy in order to make easy the way to validate data

--- app/Policies/LabelPolicy.php
+++ app/Policies/LabelPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Classes\ValidatePolicy;
 use App\Models\Label;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -11,10 +12,42 @@ class LabelPolicy
 {
     use HandlesAuthorization;
 
+    protected ValidatePolicy $validatePolicy;
+
+    public function __construct(ValidatePolicy $validatePolicy)
+    {
+        $this->validatePolicy = $validatePolicy;
+    }
+
     public function before(User $user, $ability)
     {
         if ($user->hasRole('admin')) {
             return true;
         }
+    }
+
+    public function viewAny(User $user): bool
+    {
+        return $this->validatePolicy->handle($user, 'read labels');
+    }
+
+    public function view(User $user, Label $label): bool
+    {
+        return $this->validatePolicy->handle($user, 'read labels');
+    }
+
+    public function create(User $user): bool
+    {
+        return $this->validatePolicy->handle($user, 'create labels');
+    }
+
+    public function update(User $user, Label $label): bool
+    {
+        return $this->validatePolicy->handle($user, 'update labels');
+    }
+
+    public function destroy(User $user, Label $label): bool
+    {
+        return $this->validatePolicy->handle($user, 'update labels');
     }
 }
