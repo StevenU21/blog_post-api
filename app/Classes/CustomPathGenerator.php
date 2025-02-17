bf48597111cd3e637709f1069b59f2e3fddcb2b2
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-17 17:59:39 -0600

    fix custom path generator

---
+++ app/Classes/CustomPathGenerator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Classes;
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    public function getPath(Media $media): string
+    {
+        $user = auth()->user();
+        $userSlug = Str::slug($user->name, '-');
+        $modelName = strtolower(class_basename($media->model));
+
+        return "{$userSlug}/{$modelName}/{$media->id}/";
+    }
+
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getPath($media) . 'conversions/';
+    }
+
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getPath($media) . 'responsive/';
+    }
+}
