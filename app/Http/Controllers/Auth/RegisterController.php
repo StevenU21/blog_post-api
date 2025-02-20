f494d9592b9ac92bd939b4de60a339aa135c8f50
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-20 17:28:07 -0600

    profile create after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
@@ -24,6 +25,10 @@ class RegisterController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        Profile::create([
+            'user_id' => $user->id
         ]);
 
         $user->assignRole('reader');
