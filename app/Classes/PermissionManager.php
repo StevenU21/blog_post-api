8f1f8c99d45146189b610ece368372e70645c68f
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-02-22 16:31:46 -0600

    add docs to permission manager class

--- app/Classes/PermissionManager.php
+++ app/Classes/PermissionManager.php
@@ -2,14 +2,48 @@
 
 namespace App\Classes;
 
+/**
+ * Class PermissionManager
+ *
+ * Manages a permission system for resources, allowing the configuration of default permissions,
+ * special permissions, and dynamic action filtering.
+ *
+ * @package App\Classes
+ */
 class PermissionManager
 {
+    /**
+     * List of basic permissions assigned to resources.
+     *
+     * @var array
+     */
     private $permissions;
+
+    /**
+     * List of permissions that have been filtered and organized.
+     *
+     * @var array
+     */
     private $filteredPermissions;
+
+    /**
+     * Special permissions that override the default permissions.
+     *
+     * @var array
+     */
     private $specialPermissions;
 
+    /**
+     * Default allowed actions on resources.
+     */
     private const DEFAULT_ACTIONS = ['read', 'create', 'update', 'destroy'];
 
+    /**
+     * Class constructor.
+     *
+     * @param array $permissions 
+     * @param array $specialPermissions
+     */
     public function __construct(array $permissions, array $specialPermissions)
     {
         $this->permissions = $permissions;
@@ -17,12 +51,16 @@ class PermissionManager
         $this->filteredPermissions = $this->buildPermissions();
     }
 
+    /**
+     * Builds the list of permissions by combining base permissions with special permissions.
+     *
+     * @return array List of filtered permissions organized by resource.
+     */
     private function buildPermissions(): array
     {
         $filtered = [];
 
         foreach ($this->permissions as $key => $value) {
-
             $resource = is_numeric($key) ? $value : $key;
             $actions = is_numeric($key) ? [] : $value;
 
@@ -40,25 +78,44 @@ class PermissionManager
         return $filtered;
     }
 
+    /**
+     * Gets the list of filtered permissions.
+     *
+     * @return array List of permissions organized by resource.
+     */
     public function get(): array
     {
         return $this->filteredPermissions;
     }
 
+    /**
+     * Removes specific permissions from the current list of permissions.
+     *
+     * @param array $remove List of permissions to remove.
+     * @return self Current instance with the updated permissions.
+     */
     public function remove(array $remove): self
     {
-        $this->filteredPermissions = array_filter($this->filteredPermissions, function ($perm) use ($remove) {
-            return !in_array($perm, $remove);
-        });
+        foreach ($remove as $r) {
+            foreach ($this->filteredPermissions as $resource => $actions) {
+                $this->filteredPermissions[$resource] = array_diff($actions, [$r]);
+            }
+        }
 
         return $this;
     }
 
+    /**
+     * Filters the current permissions to include only the specified ones.
+     *
+     * @param array $only List of permissions to retain.
+     * @return self Current instance with the filtered permissions.
+     */
     public function only(array $only): self
     {
-        $this->filteredPermissions = array_filter($this->filteredPermissions, function ($perm) use ($only) {
-            return in_array($perm, $only);
-        });
+        foreach ($this->filteredPermissions as $resource => $actions) {
+            $this->filteredPermissions[$resource] = array_intersect($actions, $only);
+        }
 
         return $this;
     }
